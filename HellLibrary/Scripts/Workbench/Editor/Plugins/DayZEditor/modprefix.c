1593bc3e7420eea41513606c70f504eb42d11843
Hell <617494454@qq.com>
2023-05-17 23:17:06 +0800

    update

--- HellLibrary/Scripts/Workbench/Editor/Plugins/DayZEditor/modprefix.c
+++ HellLibrary/Scripts/Workbench/Editor/Plugins/DayZEditor/modprefix.c
@@ -2,7 +2,7 @@ static string g_ModPrefix;
 
 static string GetWorkDriveDirectory() {
 	
-	g_ModPrefix = "P:\\HellLibrary\\Workbench\\";
+	g_ModPrefix = "P:\\HellLibrary\\Scripts\\Workbench\\";
 	
 	return g_ModPrefix;
 }
